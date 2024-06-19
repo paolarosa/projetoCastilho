d51271b93821513121a7f9e7d545c8a6ad89d621
Paola Lucio Rosa <paola.rosa@castilho.com.br>
2024-06-19 15:48:04 -0300

    feat: add responsive code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const sliderWrapper = document.querySelector(".slider-wrapper");
     const services = document.querySelectorAll(".service");
 
@@ -8,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
         sliderWrapper.appendChild(clone);
     });
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const sliderWrapper = document.querySelector(".slider-wrapper");
+//     const services = document.querySelectorAll(".service");
+//     const numberOfClones = Math.ceil(sliderWrapper.offsetWidth / (services[0].offsetWidth + 20)); // Calcula o número de clones necessários
+
+//     services.forEach(service => {
+//         for (let i = 0; i < numberOfClones; i++) {
+//             const clone = service.cloneNode(true);
+//             sliderWrapper.appendChild(clone);
+//         }
+//     });
+// });
+
